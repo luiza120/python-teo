6ad42d5611b630d8194ee878f2a05e8dda117ec5
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-23 07:43:13 -0300

    ATT

---
+++ app.py
@@ -0,0 +1,11 @@
+print(""" 
+      𝓔𝓼𝓽𝓾́𝓭𝓲𝓸 𝓕𝓸𝓽𝓸́𝓰𝓻𝓪𝓯𝓲𝓬𝓸 𝓛𝓩 
+      """);
+
+print('1. Cadastrar Fotógrafos')
+print('2. Listar Clientes')
+print('3. Ativar Agenda')
+print('4. Sair')
+
+opcao_escolhida = input("Escolha uma opção: ")
+print('Você escolheu a opção: ', opcao_escolhida)
