f8b3907f3caac02113dad05cbe166e801519d8f7
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-27 11:26:36 -0300

    att

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
-print(""" 
-      𝓔𝓼𝓽𝓾́𝓭𝓲𝓸 𝓕𝓸𝓽𝓸́𝓰𝓻𝓪𝓯𝓲𝓬𝓸 𝓛𝓩 
-      """);
+import os
 
-print('1. Cadastrar Fotógrafos')
-print('2. Listar Clientes')
-print('3. Ativar Agenda')
-print('4. Sair')
+def mostra_titulo():
+      print(""" 
+            𝓔𝓼𝓽𝓾́𝓭𝓲𝓸 𝓕𝓸𝓽𝓸́𝓰𝓻𝓪𝓯𝓲𝓬𝓸 𝓛𝓩 
+            """);
 
-opcao_escolhida = input("Escolha uma opção: ")
-print('Você escolheu a opção: ', opcao_escolhida)
+def mostra_escolhas():
+      print('1. Cadastrar Fotógrafos')
+      print('2. Listar Clientes')
+      print('3. Ativar Agenda')
+      print('4. Sair')
+
+def escolhe_opcao():
+
+      def finalizar_programa():
+            os.system('cls')
+            print('finalizando o programa\n')
+
+      def opcao_invalida():
+            print('Esse carácter não é permitido\n')
+            input('Aperte qualquer tecla para voltar')
+            main()
+
+      try:
+            opcao_escolhida = int(input("Escolha uma opção: "))
+
+            if opcao_escolhida == 1:
+                  print('Você escolheu Cadastrar Fotógrafos')
+            elif opcao_escolhida == 2:
+                  print('Você escolheu Listar Clientes')
+            elif opcao_escolhida == 3:
+                  print('Você escolheu Ativar Agenda')
+            elif opcao_escolhida == 4:
+                  finalizar_programa()
+            else:
+                  opcao_invalida()
+      except:
+            opcao_invalida()
+
+def main ():
+      mostra_titulo()
+      mostra_escolhas()
+      escolhe_opcao()
+
+if __name__ == '__main__':
+            main()
+
+
+
+
+
+
+
