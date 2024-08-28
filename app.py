bba59affa420b7189705fa1879c7fa477dfda2fe
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-08-28 11:58:02 -0300

    att

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 
+fotografos = [{'nome':'Luana Jazmin', 'categoria': 'moda', 'ativo':True},
+              {'nome':'Taylor Swift', 'categoria': 'social', 'ativo':True},
+              {'nome':'Igor Pires', 'categoria': 'retratista', 'ativo':False}]
+
 def mostra_titulo():
       print(""" 
             𝓔𝓼𝓽𝓾́𝓭𝓲𝓸 𝓕𝓸𝓽𝓸́𝓰𝓻𝓪𝓯𝓲𝓬𝓸 𝓛𝓩 
@@ -13,9 +17,37 @@ def mostra_escolhas():
 
 def escolhe_opcao():
 
+      def exibir_subtitulo(texto):
+            os.system('cls')
+            print(texto)
+            print('')
+
+      def retorna_menu():
+            input(' Digite uma tecla para voltar ao menu principal')
+            main()
+
+
+      def cadastra_fotografo():
+            exibir_subtitulo('Cadastrar fotografo')
+
+            nome_fotografo = input('Digite o nome do fotógrafo que deseja cadastrar:')
+            fotografos.append(nome_fotografo)
+            print(f'O fotógrafo {nome_fotografo} foi cadastrado com sucesso\n')
+            retorna_menu()
+
+      def listar_clientes():
+            exibir_subtitulo('Lista de clientes cadastrados')
+
+            for fotografo in fotografos:
+                  nome_fotografo = fotografo['nome']
+                  categoria_fotografo = fotografo['categoria']
+                  ativo = fotografo['ativo']
+                  print(f' - {nome_fotografo} | {categoria_fotografo} | {ativo}')
+            retorna_menu()
+
       def finalizar_programa():
             os.system('cls')
-            print('finalizando o programa\n')
+            print('Finalizando o programa\n')
 
       def opcao_invalida():
             print('Esse carácter não é permitido\n')
@@ -26,9 +58,9 @@ def escolhe_opcao():
             opcao_escolhida = int(input("Escolha uma opção: "))
 
             if opcao_escolhida == 1:
-                  print('Você escolheu Cadastrar Fotógrafos')
+                  cadastra_fotografo()
             elif opcao_escolhida == 2:
-                  print('Você escolheu Listar Clientes')
+                  listar_clientes()
             elif opcao_escolhida == 3:
                   print('Você escolheu Ativar Agenda')
             elif opcao_escolhida == 4:
@@ -44,7 +76,7 @@ def main ():
       escolhe_opcao()
 
 if __name__ == '__main__':
-            main()
+      main()
 
 
 
